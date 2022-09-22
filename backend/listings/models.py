ba3bc4b1376a0169526b85b557a82906d1339980
ZachKetner <87997905+ZachKetner@users.noreply.github.com>
2022-09-22 16:08:37 -0500

    about 75% complete

--- backend/listings/models.py
+++ backend/listings/models.py
@@ -3,9 +3,12 @@ from tokenize import blank_re
 from django.contrib.gis.db import models
 from django.utils import timezone
 from django.contrib.gis.geos import Point
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class Listing(models.Model):
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null = True)
     title = models.CharField(max_length=150)
     description = models.TextField(null = True, blank=True)
     choices_area = (
@@ -39,7 +42,8 @@ class Listing(models.Model):
     elevator = models.BooleanField(default=False)
     parking = models.BooleanField(default=False)
     date_posted = models.DateTimeField(default=timezone.now)
-    location = models.PointField(blank=True, null=True, srid=4326)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
     picture1 = models.ImageField(blank=True, null=True, upload_to="pictures/%Y/%m/%d/")
     picture2 = models.ImageField(blank=True, null=True, upload_to="pictures/%Y/%m/%d/")
     picture3 = models.ImageField(blank=True, null=True, upload_to="pictures/%Y/%m/%d/")
